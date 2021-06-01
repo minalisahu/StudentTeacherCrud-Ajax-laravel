51145332098889700972e19677e5680c8989fc73
Minali <minali.sahu@glocalview.com>
2021-06-01 22:43:32 +0530

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,3 @@ require('./bootstrap');
 
 require('alpinejs');
 
-.required-label:after {
-    content: "*";
-    color: red;
-}
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -24,7 +24,7 @@
                     </x-jet-nav-link>
                 </div>
                  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('student.list') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('student.list') }}" :active="request()->routeIs('students')">
                         {{ __('Students') }}
                     </x-jet-nav-link>
                 </div>
